d627d4d5f5e76af9ce952279d5dde75ddd0f42e3
starsharp06sharp <zhenglei@std.uestc.edu.cn>
2016-06-18 01:13:35 +0800

    ES6支持

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@
 var gulp = require('gulp');
 var uglify = require("gulp-uglify");
 var browserify = require('browserify');
-var reactify = require('reactify'); 
+var babelify = require('babelify'); 
 var source = require('vinyl-source-stream');
 var streamify = require('gulp-streamify');
 
 gulp.task('build', function () {
     var b = browserify({
         entries: './src/app.jsx',
-        transform: [reactify]
+        transform: [babelify.configure({ presets: ['react', 'es2015'] })]
     });
     return b.bundle()
             .pipe(source('app.js'))
